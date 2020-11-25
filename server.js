9b59773217c02c25620b904244b390f23fad61fd
Jack McNicol <jack.mcnicol@aligent.com.au>
2020-11-25 15:16:03 +1030

    update routes

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 // We need to use sessions to keep track of our user's login status
-app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(session({ secret: process.env.SECRET, resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -26,12 +26,15 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // Requiring our routes
-require('./routes/html-routes.js')(app);
-require('./routes/api-routes.js')(app);
+const htmlRoutes = require('./routes/html-routes.js');
+const apiRoutes = require('./routes/api-routes.js');
+
+app.use(htmlRoutes);
+app.use(apiRoutes);
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log('==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.', PORT, PORT);
-  });
+db.sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => {
+        console.log('==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.', PORT, PORT);
+    });
 });
