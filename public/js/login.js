4a6dd217049856fbb050cb823d90f4a28d6d59ee
Jack McNicol <jack.mcnicol@aligent.com.au>
2020-11-25 15:18:17 +1030

    update fe scripts

--- public/js/login.js
+++ public/js/login.js
@@ -1,39 +1,35 @@
-$(document).ready(function() {
-  // Getting references to our form and inputs
-  var loginForm = $("form.login");
-  var emailInput = $("input#email-input");
-  var passwordInput = $("input#password-input");
+/* global toastMessage */
+// Getting references to our form and inputs
+const loginForm = $('form.login');
 
-  // When the form is submitted, we validate there's an email and password entered
-  loginForm.on("submit", function(event) {
+// When the form is submitted, we validate there's an email and password entered
+loginForm.on('submit', (event) => {
     event.preventDefault();
-    var userData = {
-      email: emailInput.val().trim(),
-      password: passwordInput.val().trim()
-    };
+    const userDataArray = $(event.currentTarget).serializeArray();
+
+    const userData = userDataArray.reduce((userData, item) => ({ ...userData, [item.name]: item.value }), {});
 
     if (!userData.email || !userData.password) {
-      return;
+        return;
     }
 
     // If we have an email and password we run the loginUser function and clear the form
-    loginUser(userData.email, userData.password);
-    emailInput.val("");
-    passwordInput.val("");
-  });
-
-  // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
-  function loginUser(email, password) {
-    $.post("/api/login", {
-      email: email,
-      password: password
-    })
-      .then(function() {
-        window.location.replace("/members");
-        // If there's an error, log the error
-      })
-      .catch(function(err) {
-        console.log(err);
-      });
-  }
+    loginUser(userData);
+    event.currentTarget.reset();
 });
+
+// loginUser does a post to our "api/login" route and if successful, redirects us the the members page
+function loginUser({ email, password }) {
+    $.post('/api/login', {
+        email: email,
+        password: password,
+    })
+        .then((userData) => {
+            toastMessage('success', `Thanks, ${userData.name}. Redirecting in 3 sec`);
+            setTimeout(() => window.location.replace('/members'), 3000);
+            // If there's an error, log the error
+        })
+        .catch((err) => {
+            toastMessage('error', err.responseText);
+        });
+}
--- public/js/members.js
+++ public/js/members.js
@@ -1,7 +1,66 @@
-$(document).ready(function() {
-  // This file just does a GET request to figure out which user is logged in
-  // and updates the HTML on the page
-  $.get("/api/user_data").then(function(data) {
-    $(".member-name").text(data.email);
-  });
+// This file just does a GET request to figure out which user is logged in
+// and updates the HTML on the page
+$.get('/api/user_data').then(function (data) {
+    const topNav = data.name
+        ? `
+        <li class="nav-item">
+            <a href="/members" class="nav-link">
+            Hi, ${data.name}
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="/logout" class="nav-link">
+            Logout
+            </a>
+        </li>
+        `
+        : `
+        <li class="nav-item">
+            <a href="/login" class="nav-link">Login</a>
+        </li>
+        `;
+    $('.js-member-update').html(topNav);
 });
+
+// Move to window to reuse.
+window.toastTemplate = (alertType, message) => {
+    let title = 'Message';
+    let alertTypeColour = '#007aff';
+    switch (alertType) {
+    case 'error':
+        title = 'Error';
+        alertTypeColour = '#dc3545';
+        break;
+    case 'success':
+        title = 'Success';
+        alertTypeColour = '#28a745';
+        break;
+
+    default:
+        // use initial
+        break;
+    }
+    return `
+<div aria-live="polite" aria-atomic="true" style="position: relative;">
+    <div class="toast show" role="alert" aria-live="assertive" aria-atomic="true" style="position: absolute; top: 20px; right: 20px;">
+        <div class="toast-header" style="min-width: 250px;">
+            <svg class="bd-placeholder-img rounded mr-2" width="20" height="20" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="${alertTypeColour}"></rect></svg>
+            <strong class="mr-auto">${title}</strong>
+            <small class="text-muted">1 sec ago</small>
+            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="toast-body">
+            ${message}
+        </div>
+    </div>
+</div>
+`;
+};
+
+window.toastMessage = (type, message) => {
+    const $alert = $('#alert');
+    const output = toastTemplate(type, message);
+    $alert.html(output);
+};
