4df70a442fac5844b6dd8eb996f3d6bcd6bef876
Jack McNicol <jack.mcnicol@aligent.com.au>
2020-11-25 15:18:05 +1030

    update models and auth

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const { Sequelize } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 
 const basename = path.basename(module.filename);
 const config = {
@@ -14,12 +14,15 @@ const db = {};
 const sequelize = new Sequelize(config.database, config.username, config.password, {
     host: config.host,
     port: config.port,
+    dialect: 'mysql',
 });
 
 fs.readdirSync(__dirname)
-    .filter((file) => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js')
+    .filter((file) => {
+        return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
+    })
     .forEach((file) => {
-        const model = sequelize.import(path.join(__dirname, file));
+        const model = require(path.join(__dirname, file))(sequelize, DataTypes);
         db[model.name] = model;
     });
 
