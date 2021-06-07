80c057b31aec2aca8129dcce1ddf89f5a07f6b79
Jack McNicol <jackmcpickle@gmail.com>
2021-06-07 20:06:36 +0930

    fix linting

--- controllers/htmlRoutes/index.js
+++ controllers/htmlRoutes/index.js
@@ -48,7 +48,7 @@ router.get('/signup', (req, res) => {
 // Here we've add our isAuthenticated middleware to this route.
 // If a user who is not logged in tries to access this route they will be redirected to the signup page
 router.get('/dashboard', withAuth, (req, res) => {
-    console.log(req.session.user)
+    console.log(req.session.user);
     res.render('dashboard', {
         user: req.session.user,
         loggedIn: req.session.loggedIn,
